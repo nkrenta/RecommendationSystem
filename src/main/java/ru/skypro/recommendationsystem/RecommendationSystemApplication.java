84b4f9b5879c30fb2751b1e5c5f7bd3f53b13a89
kit <nkrenta@inbox.ru>
2026-06-09 16:46:23 +0300

    init

---
+++ src/main/java/ru/skypro/recommendationsystem/RecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.recommendationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationSystemApplication.class, args);
+    }
+
+}
